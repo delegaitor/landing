fc30df5d1f265c17e3ce51cb57979d89c29c8487
bobetbat <b.bobetayev.97@gmail.com>
2024-08-15 22:41:04 +0200

    chore: init

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  APP_URL: process.env.FRONTEND_URL || "http://localhost:3000",
+  LANDING_URL: process.env.NEXT_PUBLIC_LANDING_URL || "http://localhost:3001",
+  PORT: process.env.PORT || 3001,
+};
+
