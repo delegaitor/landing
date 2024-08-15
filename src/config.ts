8a19e646f1c2e9ca43271bae3b15942163b0e878
bobetbat <b.bobetayev.97@gmail.com>
2024-08-15 22:47:46 +0200

    fix: url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  APP_URL: process.env.FRONTEND_URL || "http://localhost:3000",
+  APP_URL: process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000",
   LANDING_URL: process.env.NEXT_PUBLIC_LANDING_URL || "http://localhost:3001",
   PORT: process.env.PORT || 3001,
 };
